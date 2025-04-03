5e7dadf0395513965e673ec9c9c628e131acf844
Mayank Kr. Jha <mayank.kr.jha.19.01@gmail.com>
2025-04-03 10:10:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "tailwindcss";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen border-red px-2">
+    <div className="flex flex-col items-center justify-center min-h-screen border-red  mx-2 px-4">
       <h1 className="text-4xl font-bold mb-4">Calculator</h1> <br />
       <Display />
     </div>
